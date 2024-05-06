9decfc489081b6bd73e678b99fd2af0089ea7555
akumar5 <ayush.kumar@transformco.com>
2024-05-06 22:47:48 +0530

    1. Added support for github to send job status 2. Added main function 3. Code changes to give dynamic contents[WIP]

--- src/pymsnotify/main.py
+++ src/pymsnotify/main.py
@@ -1,10 +1,27 @@
+from turtle import color
+
 import requests
 import os
+from datetime import datetime
 
+datetime_now = datetime.now()
+today_date = datetime_now.strftime("%m-%d-%Y")
 # get the input
 url = os.environ.get("URL")
-content = os.environ.get("CONTENT")
+msg = os.environ.get("MSG")
 title = os.environ.get("TITLE")
+status = os.environ.get("STATUS")
+github_server_url = os.environ.get("GITHUB_SERVER_URL", default="https://github.com")
+github_repository = os.environ.get("GITHUB_REPOSITORY")
+github_run_id = os.environ.get("GITHUB_RUN_ID")
+
+
+def main():
+    GITHUB_ACTIONS_URL = f"{github_server_url}/{github_repository}/actions/runs/{github_run_id}/"
+    content = f"""<a href="{GITHUB_ACTIONS_URL}" target="_blank">[❌] Github Actions | Workflow Failure | Repository: {github_repository} | {today_date}</a>"""
+    print(status, type(status))
+    send_teams(url, content, title)
+
 
 def send_teams(webhook_url:str, content:str, title:str, color:str="000000") -> int:
     """
@@ -28,5 +45,3 @@ def send_teams(webhook_url:str, content:str, title:str, color:str="000000") -> i
         },
     )
     return response
-
-send_teams(url, content, title)
